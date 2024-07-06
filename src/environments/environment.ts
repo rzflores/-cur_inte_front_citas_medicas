14c4aff806bfa709e2d3cbd66aa7ee0f6dff5e28
Renzo Flores Chacon <58085792+rzflores@users.noreply.github.com>
2024-07-06 17:24:57 -0500

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-    //apiUrl: 'https://curintebackcitasmedicas-production.up.railway.app',
-    apiUrl: 'http://localhost:4200',
+    apiUrl: 'https://curintebackcitasmedicas-production.up.railway.app',
+    //apiUrl: 'http://localhost:4200',
   };
